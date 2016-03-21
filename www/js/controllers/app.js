1d6964c6f7c9f2d0b782336a24dedd99bc27fa92
undefined undefined <DieselCon@users.noreply.github.com>
2016-03-21 13:03:40 +0000

    Add right menu and update keyboard plugin.

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,7 +1,14 @@
 'use strict';
 /* global angular, Bitcoin */
-angular.module('carbonkey.controllers').controller('AppController', function($scope, $ionicSideMenuDelegate) {
+angular.module('carbonkey.controllers').controller('AppController', 
+  function($scope, $ionicSideMenuDelegate) {
+    
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
+    
+  $scope.toggleRight = function() {
+    $ionicSideMenuDelegate.toggleRight();
+  };
+  
 })
